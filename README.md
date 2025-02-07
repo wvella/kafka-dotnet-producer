d31132c48eb4615990cb28335d356cb38b292f52
Warren Vella <warren.vella18@gmail.com>
2025-02-07 16:18:20 +1100

    Initial commit of code

---
+++ README.md
@@ -0,0 +1,68 @@
+# .NET load testing producer
+
+This project contains a .NET application that produces records to a topic on Confluent Cloud and prints the produced records to the console.
+
+This has been built to play around with the batching properties. In my example, I have deployed a Standard Cluster in Confluent Cloud, with 1 Topic, 6 partitions, with a `max.message.bytes` of `7340032`.
+
+The producer is configured with the following properties:
+
+```
+    "LingerMs": 500,
+    "BatchNumMessages": 4000,
+    "MessageMaxBytes": 6291456,
+    "BatchSize": 6291456
+```
+
+## Getting Started
+
+### Prerequisites
+
+.NET Core (>= 8.0) installed.
+
+### Usage
+
+Build the producer by running:
+
+```shell
+$ dotnet build producer.csproj
+```
+
+Run the producer by running this from the src/ directory:
+
+```shell
+$ dotnet run
+```
+
+### App Configuration
+
+Create a appsettings.json file in the root as below:
+
+```json
+{
+    "Kafka": {
+      "BootstrapServers": "<<bootstrap name>>:<<bootstrap port>>",
+      "SaslUsername": "<<API Key>>",
+      "SaslPassword": "<<API Secret>>",
+      "LingerMs": 500,
+      "BatchNumMessages": 4000,
+      "MessageMaxBytes": 6291456,
+      "BatchSize": 6291456,
+      "Debug": "msg"
+    },
+    "SchemaRegistry": {
+      "Url": "https://<<schema registry url>>",
+      "BasicAuthUserInfo": "<<API Key>>:<<API Secret>>"
+    },
+    "Producer": {
+      "numOfMessages": 400000,
+      "sizeOfPayload": 2048
+    },
+    "_comments": {
+      "note1": "1048576 = 1MB",
+      "note2": "2097152 = 2MB",
+      "note3": "4194304 = 4MB",
+      "note4": "6291456 = 6MB",
+      "note5": "7340032 = 7MB"
+    }
+  }
+```
---
+++ producer.cs
@@ -0,0 +1,110 @@
+// Copyright 2018 Confluent Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Refer to LICENSE for more information.
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.Extensions.Configuration;
+using Confluent.Kafka.SyncOverAsync;
+
+namespace Confluent.Kafka.Examples.AvroSpecific
+{
+    class Producer
+    {
+        public static void Main(string[] args)
+        {
+            var stopwatchMain = Stopwatch.StartNew();
+            const string topicName = "raw.inventory";
+
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Read Kafka configuration
+            var kafkaConfig = configuration.GetSection("Kafka");
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = kafkaConfig["BootstrapServers"],
+                SaslUsername = kafkaConfig["SaslUsername"],
+                SaslPassword = kafkaConfig["SaslPassword"],
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.Plain,
+                Acks = Acks.All,
+                Debug = kafkaConfig["debug"],
+                LingerMs = int.Parse(kafkaConfig["LingerMs"]),
+                BatchNumMessages = int.Parse(kafkaConfig["BatchNumMessages"]),
+                MessageMaxBytes = int.Parse(kafkaConfig["MessageMaxBytes"]),
+                BatchSize = int.Parse(kafkaConfig["BatchSize"])
+            };
+
+            // Read Schema Registry configuration
+            var schemaRegistryConfig = new SchemaRegistryConfig
+            {
+                Url = configuration["SchemaRegistry:Url"],
+                BasicAuthUserInfo = configuration["SchemaRegistry:BasicAuthUserInfo"]
+            };
+
+            var avroSerializerConfig = new AvroSerializerConfig
+            {
+                // optional Avro serializer properties:
+                BufferBytes = 100
+            };
+
+            using (var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig))
+
+            using (var p = new ProducerBuilder<string, User>(producerConfig)
+            .SetValueSerializer(new AvroSerializer<User>(schemaRegistry, avroSerializerConfig).AsSyncOverAsync())
+            .Build())
+            {
+                Console.WriteLine($"{p.Name} producing on {topicName}.");
+
+                int i = 1;
+                int numOfMessages = int.Parse(configuration["Producer:numOfMessages"]);
+                int sizeOfPayload = int.Parse(configuration["Producer:sizeOfPayload"]);
+                for (int j = 0; j < numOfMessages; j++)
+                {
+                    User user = new User { name = "user:" + j, favorite_color = new string('g', sizeOfPayload), favorite_number = ++i, hourly_rate = new Avro.AvroDecimal(67.99) };
+                    try
+                    {
+                        // Example 1 - Produce - Still Async with a callback
+                        p.Produce(topicName, new Message<string, User> { Key = "user-" + j, Value = user });
+                    }
+                    catch (ProduceException<string, User> e) when (e.Error.Code == ErrorCode.Local_QueueFull)
+                    {
+                        Console.WriteLine($"Local Delivery Queue full. Retrying... {e.Message}");
+                        Console.WriteLine(j);
+                        p.Poll(TimeSpan.FromMilliseconds(2000)); // https://support.confluent.io/hc/en-us/articles/360053146092-How-to-bypass-BufferError-Local-Queue-full-in-librdkafka-producers
+                    }
+                    catch (Exception e) // Catch all other exceptions
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Message}");
+                    }
+                }
+                // wait for up to 100 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(100));
+            }
+            stopwatchMain.Stop();
+            Console.WriteLine($"Total execution time: {stopwatchMain.Elapsed}");
+        }
+
+    }
+}
